dc97602717a825925143b9771237d12041dab43b
Austin Reimer <austin.d.reimer@gmail.com>
2021-03-30 12:03:38 -0500

    Added a anonymous function

--- primecounter_vlang.v
+++ primecounter_vlang.v
@@ -3,14 +3,17 @@ import math
 
 fn main() {
 	sw := time.new_stopwatch({})
-	mut results := []int{}
+	mut count := 0
+	mut result_recive := 0
 	for {
-		results << run(1000000)
+		result_recive = run(1_000_000)
+		count ++
 		if sw.elapsed().seconds() > 5 {
 			break
 		}
 	}
-	println(results.len)
+	println(count)
+	println(result_recive)
 
 
 
@@ -24,12 +27,21 @@ fn main() {
 }
 
 fn run(maxnum int) int {
-	mut rawbits := []bool{len: int((maxnum + 1) /2), init: true}
+	mut rawbits := []bool{len: int((maxnum + 1) / 2),init: true}
 	mut count := 0
 
+	get_odd := fn (number int, rawbit bool) bool {
+		if number % 2 == 0 {
+			return false
+		}
+		else {
+			return rawbit
+		}
+	}
+
 	for facter := 3; facter < math.sqrt(maxnum); facter += 2 {
-		for num in facter..maxnum {
-			if get_odd(num, rawbits) {
+		for num := facter; num < maxnum; num += 2 {
+			if get_odd(num, rawbits[num / 2]) {
 				facter = num
 				break
 			}
@@ -46,13 +58,4 @@ fn run(maxnum int) int {
 	}
 
 	return count
-}
-
-fn get_odd(number int, rawbits []bool) bool {
-	if number % 2 == 0 {
-		return false
-	}
-	else {
-		return rawbits[int(number /2)]
-	}
 }
